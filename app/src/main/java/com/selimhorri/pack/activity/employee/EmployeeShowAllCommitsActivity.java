e35967686b9846ea2e1c80cc96f9b6e53fe71a16
Selim Horri <horri.slim@gmail.com>
2021-06-27 19:51:19 +0100

    impl employee show all commits correctly

--- app/src/main/java/com/selimhorri/pack/activity/employee/EmployeeShowAllCommitsActivity.java
+++ app/src/main/java/com/selimhorri/pack/activity/employee/EmployeeShowAllCommitsActivity.java
@@ -1,22 +1,71 @@
 package com.selimhorri.pack.activity.employee;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.selimhorri.pack.R;
 import com.selimhorri.pack.activity.HomeActivity;
+import com.selimhorri.pack.pattern.adapter.EmployeeShowAllCommitsCustomAdapter;
+import com.selimhorri.pack.service.AssignmentService;
+import com.selimhorri.pack.service.EmployeeService;
+import com.selimhorri.pack.service.impl.dynmc.AssignmentServiceDynamicImpl;
+import com.selimhorri.pack.service.impl.dynmc.EmployeeServiceDynamicImpl;
 
 public class EmployeeShowAllCommitsActivity extends AppCompatActivity {
+
+    private final EmployeeService employeeService;
+    private final AssignmentService assignmentService;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    public EmployeeShowAllCommitsActivity() {
+        this.employeeService = new EmployeeServiceDynamicImpl(EmployeeShowAllCommitsActivity.this);
+        this.assignmentService = new AssignmentServiceDynamicImpl(EmployeeShowAllCommitsActivity.this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_show_all_commits);
+
+        this.recyclerView = super.findViewById(R.id.recyclerViewEmployeeShowAllCommits);
+        this.recyclerView.setHasFixedSize(true);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(EmployeeShowAllCommitsActivity.this));
+
+        final Bundle extras = super.getIntent().getExtras();
+        final Integer projectId = extras.getInt("projectId");
+
+        final SharedPreferences sp = super.getSharedPreferences("emp", MODE_PRIVATE);
+        final String username = sp.getString("username", null);
+
+        this.employeeService.findByUsername(
+                username,
+                response -> {
+                    this.assignmentService.findByProjectId(
+                            projectId,
+                            list -> {
+                                this.recyclerView.setAdapter(
+                                        new EmployeeShowAllCommitsCustomAdapter(
+                                                EmployeeShowAllCommitsActivity.this,
+                                                list.getCollection()
+                                        )
+                                );
+                            },
+                            errorList -> Toast.makeText(EmployeeShowAllCommitsActivity.this, errorList.toString(), Toast.LENGTH_SHORT).show()
+                    );
+                },
+                error -> Toast.makeText(EmployeeShowAllCommitsActivity.this, error.toString(), Toast.LENGTH_SHORT).show()
+        );
+
     }
 
     @Override
--- app/src/main/java/com/selimhorri/pack/pattern/adapter/EmployeeShowCommitsCustomAdapter.java
+++ app/src/main/java/com/selimhorri/pack/pattern/adapter/EmployeeShowCommitsCustomAdapter.java
@@ -1,7 +1,6 @@
 package com.selimhorri.pack.pattern.adapter;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
